de1cccec2b3a54a9f38aba36e82587c24f29f2ae
Vladimir Shtefan <shtefanvladimir14@gmail.com>
2022-04-24 13:00:41 +0700

    changed the algorithm

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,13 +1,16 @@
 def search_bracket(user_string):
-    user_list = list(user_string)
-    while True:
-        if '(' in user_list and ')' in user_list:
-            if user_list.count('(') == user_list.count(')'):
+    list_open_bracket = []
+    list_close_bracket = []
+    for number_symbol in range(len(user_string)):
+        if user_string[number_symbol] == '(':
+            list_open_bracket.append(number_symbol)
+        elif user_string[number_symbol] == ')':
+            list_close_bracket.append(number_symbol)
+    if len(list_open_bracket) == len(list_close_bracket):
+        if list_open_bracket[0] < list_close_bracket[0]:
+            if list_close_bracket[len(list_close_bracket)-1] > list_open_bracket[len(list_open_bracket)-1]:
                 return 'Верно'
-            else:
-                return 'Не верно'
-        else:
-            return 'Не верно'
+    return 'Не верно'
 
 
 print(search_bracket(input()))
